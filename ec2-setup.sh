2db15f2fb78e311cf47c2d4be10a192c79b307c1
Sri Vidya <indupv@hotmail.com>
2020-01-17 14:45:28 -0500

    added spinner

--- ec2-setup.sh
+++ ec2-setup.sh
@@ -1,1 +1,1 @@
-sudo yum -y update && sudo yum install -y git python3 tmux htop nano gcc python3-devel.x86_64  
+sudo yum -y update && sudo yum install -y git python3 tmux htop nano gcc python3-devel.x86_64 
